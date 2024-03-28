40d188a6f35cb1819d6b187243152b708b493698
Rafael Fujii <50469986+RafaelFujii677@users.noreply.github.com>
2024-03-28 10:13:54 -0300

    add unit test to findAll userService

--- src/test/java/br/com/webfluxcourse/services/UserServiceTest.java
+++ src/test/java/br/com/webfluxcourse/services/UserServiceTest.java
@@ -5,8 +5,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
-import java.util.Objects;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +16,7 @@ import br.com.webfluxcourse.entities.User;
 import br.com.webfluxcourse.mapper.UserMapper;
 import br.com.webfluxcourse.model.request.UserRequest;
 import br.com.webfluxcourse.repositories.UserRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -49,15 +48,29 @@ public class UserServiceTest {
 
 	@Test
 	void testFindOneById() {
-		when(repository.findOneById(anyString())).thenReturn(Mono.just(User.builder().id("1234").build()));
+		when(repository.findOneById(anyString())).thenReturn(Mono.just(User.builder().build()));
 
 		Mono<User> result = service.findOneById("123");
 
 		StepVerifier.create(result)
-			.expectNextMatches(user -> user.getClass() == User.class && user.getId() == "1234")
+			.expectNextMatches(user -> user.getClass() == User.class)
 			.expectComplete()
 			.verify();
 
 		Mockito.verify(repository, times(1)).findOneById(anyString());
+	}
+
+	@Test
+	void testFindAll() {
+		when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+		Flux<User> result = service.findAll();
+
+		StepVerifier.create(result)
+			.expectNextMatches(user -> user.getClass() == User.class)
+			.expectComplete()
+			.verify();
+
+		Mockito.verify(repository, times(1)).findAll();
 	}
 }
