5b781d871c5f2c634cb6f409edab17eac5c21e1d
Rafael Fujii <50469986+RafaelFujii677@users.noreply.github.com>
2024-04-02 11:12:59 -0300

    add integration to findOneById endpoint with success

--- src/test/java/br/com/webfluxcourse/controller/UserControllerImplTest.java
+++ src/test/java/br/com/webfluxcourse/controller/UserControllerImplTest.java
@@ -1,6 +1,7 @@
 package br.com.webfluxcourse.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -16,8 +17,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+
 import br.com.webfluxcourse.entities.User;
+import br.com.webfluxcourse.mapper.UserMapper;
 import br.com.webfluxcourse.model.request.UserRequest;
+import br.com.webfluxcourse.model.response.UserResponse;
 import br.com.webfluxcourse.services.UserService;
 
 @SpringBootTest
@@ -28,6 +33,8 @@ public class UserControllerImplTest {
 	private WebTestClient webTestClient;
 
 	@MockBean private UserService service;
+	@MockBean private UserMapper mapper;
+	@MockBean private MongoClient mongoClient;
 
 	@Test
 	@DisplayName("Test endpoint save with success")
@@ -62,6 +69,24 @@ public class UserControllerImplTest {
 			.jsonPath("$.message").isEqualTo("Error on validation attributes")
 			.jsonPath("$.errors[0].fieldName").isEqualTo("name")
 			.jsonPath("$.errors[0].message").isEqualTo("field cannot have blank spaces at the beginning or at end");
+	}
 
+	@Test
+	@DisplayName("Test find by id endpoint with success")
+	void findByIdWithSuccess() {
+		final var id = "123456";
+		final var userResponse = new UserResponse(id, "Valdir", "valdir@gmail.com", "123");
+		when(service.findOneById(anyString())).thenReturn(just(User.builder().build()));
+		when(mapper.toResponse(any(User.class))).thenReturn(userResponse);
+
+		webTestClient.get().uri("/users/" + id)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.id").isEqualTo(id)
+			.jsonPath("$.name").isEqualTo("Valdir")
+			.jsonPath("$.email").isEqualTo("valdir@gmail.com")
+			.jsonPath("$.password").isEqualTo("123");
 	}
 }
